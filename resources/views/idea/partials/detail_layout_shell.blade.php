4672aea98ada487587d3459c6d73f223595eae70
Ross t <ross.tweedie@elixirr.com>
2026-05-06 10:28:30 +0100

    refactor(detail): harden shared shell rendering

--- resources/views/idea/partials/detail_layout_shell.blade.php
+++ resources/views/idea/partials/detail_layout_shell.blade.php
@@ -3,19 +3,33 @@
 @endphp
 
 <div class="max-w-6xl mx-auto px-6 md:px-8 pt-16 pb-24 space-y-6">
-    {!! $header ?? '' !!}
+    @isset($header)
+        {{ $header }}
+    @endisset
 
     @if ($twoColumn)
         <div class="grid gap-6 lg:grid-cols-[minmax(0,2fr)_minmax(18rem,1fr)] lg:items-start">
             <div class="space-y-6 min-w-0" data-thought-detail-main>
-                {!! $main ?? '' !!}
+                @isset($main)
+                    {{ $main }}
+                @endisset
             </div>
 
-            {!! $sidebar ?? '' !!}
+            <div class="space-y-6">
+                @isset($sidebar)
+                    {{ $sidebar }}
+                @endisset
+            </div>
         </div>
     @else
-        {!! $main ?? '' !!}
+        <div class="space-y-6">
+            @isset($main)
+                {{ $main }}
+            @endisset
+        </div>
     @endif
 
-    {!! $footer ?? '' !!}
+    @isset($footer)
+        {{ $footer }}
+    @endisset
 </div>
